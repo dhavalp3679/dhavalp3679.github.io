8e55323dc2f4b9c90bf6beb1e08c96315b7b1a1b
Sagar Sahu <87543042+rsagar024@users.noreply.github.com>
2023-05-04 17:37:29 +0530

    Swap the number

--- main.py
+++ main.py
@@ -7,3 +7,17 @@ random_number = random.randint(0, 9)
 
 print(random_number)
 
+# take input from the user
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+# before swapping
+print("Before swapping: a =", a, "and b =", b)
+
+# swap the values
+temp = a
+a = b
+b = temp
+
+# after swapping
+print("After swapping: a =", a, "and b =", b)
